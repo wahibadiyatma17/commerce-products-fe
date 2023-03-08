3637f9bddb5cbb112bc2e8bc2536d2b5520961f2
wahib-sirka <wahib@sirka.io>
2023-03-08 22:27:19 +0700

    chore(products feature): remove unused console log

--- components/Container/Homepage/index.tsx
+++ components/Container/Homepage/index.tsx
@@ -1,15 +1,15 @@
-import React, { FC, useMemo, useState } from 'react';
-import { Input } from 'alceo-design';
-import { BiSearch as SearchIcon } from 'react-icons/bi';
-import { useDebounce } from 'usehooks-ts';
-import tw, { styled } from 'twin.macro';
+import React, { FC, useMemo, useState } from "react";
+import { Input } from "alceo-design";
+import { BiSearch as SearchIcon } from "react-icons/bi";
+import { useDebounce } from "usehooks-ts";
+import tw, { styled } from "twin.macro";
 
-import { useGetProducts } from 'hooks/products/productsHooks';
-import ProductCard from './components/ProductCard';
-import { productDataProps } from 'types/productsType';
+import { useGetProducts } from "hooks/products/productsHooks";
+import ProductCard from "./components/ProductCard";
+import { productDataProps } from "types/productsType";
 
 const Homepage: FC = () => {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>("");
 
   // use debounce search to prevent hitting a lot API on every keystroke change.
   const debouncedSearch = useDebounce(value, 500);
@@ -20,29 +20,16 @@ const Homepage: FC = () => {
 
   const { data: productsRes, isLoading: isProductsLoading } = useGetProducts(params);
   const productsData = useMemo(() => productsRes?.data ?? [], [productsRes]);
-  console.log('wahib productsData', productsData);
 
   return (
     <div tw="flex flex-col w-full h-full min-h-screen items-center max-w-[1366px] mx-auto gap-8 py-10 px-[4.5rem]">
       <h4 tw="font-semibold text-xl">Daftar Produk</h4>
       <StyledSearchInput>
-        <Input
-          id="filterGlobal"
-          type="text"
-          placeholder="Cari Produk"
-          suffix={<SearchIcon />}
-          defaultValue={value}
-          onChange={(e) => setValue(e?.target?.value)}
-        />
+        <Input id="filterGlobal" type="text" placeholder="Cari Produk" suffix={<SearchIcon />} defaultValue={value} onChange={(e) => setValue(e?.target?.value)} />
       </StyledSearchInput>
       <div tw="flex gap-8 flex-wrap max-w-[80%] items-center justify-center">
         {productsData.map((data: productDataProps, idx: number) => (
-          <ProductCard
-            title={data.title}
-            price={data.price}
-            description={data.description}
-            key={idx}
-          />
+          <ProductCard title={data.title} price={data.price} description={data.description} key={idx} />
         ))}
       </div>
     </div>
